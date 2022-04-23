7e291da4e20f59be82faf639cea66c1016cf4695
Tyler <tyler.jankowski@hotmail.com>
2022-04-23 12:05:52 -0400

    added new about navbar and options

---
+++ src/app/components/about-navbar/about-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-navbar',
+  templateUrl: './about-navbar.component.html',
+  styleUrls: ['./about-navbar.component.css']
+})
+export class AboutNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
